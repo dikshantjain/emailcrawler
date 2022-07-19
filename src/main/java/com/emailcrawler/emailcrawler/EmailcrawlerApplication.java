b7e19dd288f1659c5127f03011f2597c29ae37d5
Dikshant Jain <dikshantjain1997@gmail.com>
2022-07-19 12:07:02 +0530

    Email crawler first commit

---
+++ src/main/java/com/emailcrawler/emailcrawler/EmailcrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.emailcrawler.emailcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailcrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailcrawler/emailcrawler/EmailcrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailcrawler.emailcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailcrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
